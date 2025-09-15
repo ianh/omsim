ianh/omsim

commit c34b318dbca84a6091054b476565fe1ed3a07ef5

Improve production handling (#18)
* Enforce production restrictions

* Fix sim failing when more than one overlapped atom exists

* Fix exit conduit timing and collisions

* Refactor apply_conduit

* Unglobalize cabinet_map

* Add check against freespace conduits

sim.c
@@ -95,11 +95,13 @@ static struct atom_ref_at_position get_atom(struct board *board, struct mechanis
 __attribute__((noinline))
 static void remove_overlapping_atom(struct board *board, struct atom_ref_at_position a)
 {
-    for (uint32_t i = 0; i < board->number_of_overlapped_atoms; --i) {
+    for (uint32_t i = 0; i < board->number_of_overlapped_atoms; ++i) {
         if (vectors_equal(board->overlapped_atoms[i].position, a.position)) {
             *a.atom = board->overlapped_atoms[i].atom;
             schedule_flag_reset_if_needed(board, a.atom);
-            memmove(board->overlapped_atoms + i, board->overlapped_atoms + i + 1, board->number_of_overlapped_atoms - i - 1);
+            memmove(board->overlapped_atoms + i,
+                    board->overlapped_atoms + i + 1,
+                    (board->number_of_overlapped_atoms - i - 1) * sizeof(struct atom_at_position));
             board->number_of_overlapped_atoms--;
             return;
         }
@@ -140,7 +142,7 @@ int direction_for_offset(struct vector d)
     return -1;
 }
 
-static int angular_distance_between_grabbers(enum mechanism_type type)
+int angular_distance_between_grabbers(enum mechanism_type type)
 {
     switch (type & ANY_ARM) {
     case ARM:
@@ -227,13 +229,14 @@ static void apply_conduit(struct solution *solution, struct board *board, struct
 {
     struct conduit *conduit = &solution->conduits[m.conduit_index];
     struct mechanism other_side = solution->glyphs[conduit->other_side_glyph_index];
-    int rotation = direction_for_offset(other_side.direction_u) - direction_for_offset(m.direction_u);
     uint32_t base = 0;
-    for (uint32_t j = 0; j < conduit->number_of_molecules; ++j) {
-        uint32_t length = conduit->molecule_lengths[j];
-        bool valid = true;
-        bool consume = true;
-        if (board->half_cycle == 1) {
+
+    if (board->half_cycle == 1) {
+        int rotation = direction_for_offset(other_side.direction_u) - direction_for_offset(m.direction_u);
+        for (uint32_t j = 0; j < conduit->number_of_molecules; ++j) {
+            uint32_t length = conduit->molecule_lengths[j];
+            bool valid = true;
+            bool consume = true;
             // if any of the atoms in this molecule have already been consumed
             // by some other glyph, then remove the molecule from the conduit.
             for (uint32_t k = 0; k < length; ++k) {
@@ -278,12 +281,10 @@ static void apply_conduit(struct solution *solution, struct board *board, struct
                 j--;
                 continue;
             }
-        }
-        for (uint32_t k = 0; k < length; ++k) {
-            atom a = conduit->atoms[base + k].atom;
-            struct vector delta = conduit->atoms[base + k].position;
-            if (board->half_cycle == 1) {
+            for (uint32_t k = 0; k < length; ++k) {
+                struct vector delta = conduit->atoms[base + k].position;
                 struct vector p = mechanism_relative_position(m, delta.u, delta.v, 1);
+                atom a = conduit->atoms[base + k].atom;
                 atom *b = lookup_atom(board, p);
                 if (consume) {
                     conduit->atoms[base + k].atom = *b;
@@ -292,13 +293,23 @@ static void apply_conduit(struct solution *solution, struct board *board, struct
                     // bonds are consumed even if the atoms aren't.
                     *b &= ~(ALL_BONDS & ~RECENT_BONDS & a);
                 }
-            } else {
-                struct vector p = mechanism_relative_position(other_side, delta.u, delta.v, 1);
+                p = mechanism_relative_position(other_side, delta.u, delta.v, 1);
                 rotate_bonds(&a, rotation);
-                insert_atom(board, p, a, "conduit output");
+                insert_atom(board, p, a | BEING_PRODUCED, "conduit output");
             }
+            base += length;
         }
-        base += length;
+    } else {
+        conduit = &solution->conduits[other_side.conduit_index];
+        for (uint32_t j = 0; j < conduit->number_of_molecules; ++j) {
+            uint32_t length = conduit->molecule_lengths[j];
+            for (uint32_t k = 0; k < length; ++k) {
+                struct vector delta = conduit->atoms[base + k].position;
+                struct vector p = mechanism_relative_position(m, delta.u, delta.v, 1);
+                *lookup_atom(board, p) &= ~BEING_PRODUCED;
+            }
+            base += length;
+        }
     }
 }
 

decode.c
@@ -215,6 +215,13 @@ static struct vector cabinet_walls_Medium[] = { {3,0}, {2,1}, {1,2}, {0,3}, {-1,
 static struct vector cabinet_walls_MediumWide[] = { {4,0}, {3,1}, {2,2}, {1,3}, {0,3}, {-1,3}, {-2,3}, {-3,3}, {-3,2}, {-3,1}, {-3,0}, {-2,-1}, {-1,-2}, {0,-3}, {1,-3}, {2,-3}, {3,-3}, {4,-3}, {4,-2}, {4,-1} };
 static struct vector cabinet_walls_Large[] = { {4,0}, {3,1}, {2,2}, {1,3}, {0,4}, {-1,4}, {-2,4}, {-3,4}, {-4,4}, {-4,3}, {-4,2}, {-4,1}, {-4,0}, {-3,-1}, {-2,-2}, {-1,-3}, {0,-4}, {1,-4}, {2,-4}, {3,-4}, {4,-4}, {4,-3}, {4,-2}, {4,-1} };
 
+static struct vector cabinet_insides_Small[] = { {0,-1}, {1,-1}, {-1,0}, {0,0}, {1,0}, {-1,1}, {0,1} };
+static struct vector cabinet_insides_SmallWide[] = { {0,-1}, {1,-1}, {2,-1}, {-1,0}, {0,0}, {1,0}, {2,0}, {-1,1}, {0,1}, {1,1} };
+static struct vector cabinet_insides_SmallWider[] = { {0,-1}, {1,-1}, {2,-1}, {3,-1}, {-1,0}, {0,0}, {1,0}, {2,0}, {3,0}, {-1,1}, {0,1}, {1,1}, {2,1} };
+static struct vector cabinet_insides_Medium[] = { {0,-2}, {1,-2}, {2,-2}, {-1,-1}, {0,-1}, {1,-1}, {2,-1}, {-2,0}, {-1,0}, {0,0}, {1,0}, {2,0}, {-2,1}, {-1,1}, {0,1}, {1,1}, {-2,2}, {-1,2}, {0,2} };
+static struct vector cabinet_insides_MediumWide[] = { {0,-2}, {1,-2}, {2,-2}, {3,-2}, {-1,-1}, {0,-1}, {1,-1}, {2,-1}, {3,-1}, {-2,0}, {-1,0}, {0,0}, {1,0}, {2,0}, {3,0}, {-2,1}, {-1,1}, {0,1}, {1,1}, {2,1}, {-2,2}, {-1,2}, {0,2}, {1,2} };
+static struct vector cabinet_insides_Large[] = { {0,-3}, {1,-3}, {2,-3}, {3,-3}, {-1,-2}, {0,-2}, {1,-2}, {2,-2}, {3,-2}, {-2,-1}, {-1,-1}, {0,-1}, {1,-1}, {2,-1}, {3,-1}, {-3,0}, {-2,0}, {-1,0}, {0,0}, {1,0}, {2,0}, {3,0}, {-3,1}, {-2,1}, {-1,1}, {0,1}, {1,1}, {2,1}, {-3,2}, {-2,2}, {-1,2}, {0,2}, {1,2}, {-3,3}, {-2,3}, {-1,3}, {0,3} };
+
 static size_t number_of_walls_for_cabinet_type(struct byte_string type)
 {
     if (byte_string_is(type, "Small"))
@@ -258,6 +265,42 @@ static size_t copy_walls_for_cabinet_type(struct byte_string type, struct vector
     return n;
 }
 
+static size_t number_of_insides_for_cabinet_type(struct byte_string type)
+{
+    if (byte_string_is(type, "Small"))
+        return sizeof(cabinet_insides_Small) / sizeof(cabinet_insides_Small[0]);
+    else if (byte_string_is(type, "SmallWide"))
+        return sizeof(cabinet_insides_SmallWide) / sizeof(cabinet_insides_SmallWide[0]);
+    else if (byte_string_is(type, "SmallWider"))
+        return sizeof(cabinet_insides_SmallWider) / sizeof(cabinet_insides_SmallWider[0]);
+    else if (byte_string_is(type, "Medium"))
+        return sizeof(cabinet_insides_Medium) / sizeof(cabinet_insides_Medium[0]);
+    else if (byte_string_is(type, "MediumWide"))
+        return sizeof(cabinet_insides_MediumWide) / sizeof(cabinet_insides_MediumWide[0]);
+    else if (byte_string_is(type, "Large"))
+        return sizeof(cabinet_insides_Large) / sizeof(cabinet_insides_Large[0]);
+    else
+        return 0;
+}
+
+static struct vector* get_insides_for_cabinet_type(struct byte_string type)
+{
+    if (byte_string_is(type, "Small"))
+        return cabinet_insides_Small;
+    else if (byte_string_is(type, "SmallWide"))
+        return cabinet_insides_SmallWide;
+    else if (byte_string_is(type, "SmallWider"))
+        return cabinet_insides_SmallWider;
+    else if (byte_string_is(type, "Medium"))
+        return cabinet_insides_Medium;
+    else if (byte_string_is(type, "MediumWide"))
+        return cabinet_insides_MediumWide;
+    else if (byte_string_is(type, "Large"))
+        return cabinet_insides_Large;
+    else
+        return 0;
+}
+
 static void mark_visible_region(struct solution *solution, struct vector p, int32_t hex_radius)
 {
     if (p.u < INT32_MIN + hex_radius)
@@ -282,6 +325,135 @@ static void mark_visible_input_output(struct solution *solution, struct input_ou
 {
     for (uint32_t i = 0; i < io->number_of_atoms; ++i)
         mark_visible_region(solution, io->atoms[i].position, 0);
+}
+
+#define MAP_SIZE 32
+
+static uint8_t cabinet_for_position(uint8_t cabinet_map[MAP_SIZE][MAP_SIZE], struct vector position)
+{
+    int32_t u = position.u + (MAP_SIZE >> 1);
+    int32_t v = position.v + (MAP_SIZE >> 1);
+    if (u >= 0 && u < MAP_SIZE && v >= 0 && v < MAP_SIZE)
+        return cabinet_map[u][v];
+    return 0;
+}
+
+static struct vector decode_position(signed char position[2])
+{
+    return (struct vector) { position[0], position[1] };
+}
+
+static char* check_production_constraints(struct solution *solution, struct puzzle_production_info *info)
+{
+    // map u,v coordinates => cabinet id + 1 (0 means no cabinet)
+    uint8_t cabinet_map[MAP_SIZE][MAP_SIZE] = { 0 };
+    for (uint32_t i = 0; i < info->number_of_cabinets; ++i) {
+        struct vector *src = get_insides_for_cabinet_type(info->cabinets[i].type);
+        size_t n = number_of_insides_for_cabinet_type(info->cabinets[i].type);
+        for (size_t j = 0; j < n; ++j) {
+            int32_t u = info->cabinets[i].position[0] + src[j].u + (MAP_SIZE >> 1);
+            int32_t v = info->cabinets[i].position[1] + src[j].v + (MAP_SIZE >> 1);
+            if (u >= 0 && u < MAP_SIZE && v >= 0 && v < MAP_SIZE)
+                cabinet_map[u][v] = i + 1;
+        }
+    }
+
+    // check conduits
+    bool swapped = false;
+    for (uint32_t i = 0; i < solution->number_of_conduits; ++i) {
+        struct conduit *conduit = &solution->conduits[i];
+        uint32_t conduit_index = conduit->id - 100; // conduit ids start at 100
+        if (conduit_index >= info->number_of_conduits)
+            return "solution contains a conduit not defined in the puzzle file";
+
+        struct puzzle_conduit *puzzle_conduit = &info->conduits[conduit_index];
+        if (conduit->number_of_positions != puzzle_conduit->number_of_hexes)
+            return "solution contains a conduit with an edited shape";
+
+        signed char *starting_position = (i & 1) ? puzzle_conduit->starting_position_b : puzzle_conduit->starting_position_a;
+        uint8_t expected_cabinet = cabinet_for_position(cabinet_map, decode_position(starting_position));
+
+        // determine if this conduit is the A-side or the B-side of its pair
+        if ((i & 1) == 0) {
+            struct vector center = solution->glyphs[conduit->glyph_index].position;
+            swapped = cabinet_for_position(cabinet_map, center) != expected_cabinet;
+        }
+        struct mechanism *m = &solution->glyphs[swapped ? conduit->other_side_glyph_index : conduit->glyph_index];
+
+        for (uint32_t j = 0; j < conduit->number_of_positions; ++j) {
+            struct vector pos = conduit->positions[j];
+            if (!vectors_equal(pos, decode_position(puzzle_conduit->hexes[j].offset)))
+                return "solution contains a conduit with an edited shape";
+            pos = mechanism_relative_position(*m, pos.u, pos.v, 1);
+            if (cabinet_for_position(cabinet_map, pos) != expected_cabinet)
+                return "solution moved a conduit outside of its original chamber";
+        }
+    }
+
+    // check glyphs
+    for (uint32_t i = 0; i < solution->number_of_glyphs; ++i) {
+        struct mechanism *m = &solution->glyphs[i];
+        if (m->type == CONDUIT)
+            continue; // conduits were already checked
+        const struct vector *footprint = glyph_footprint(m->type);
+        for (int j = 0; ; ++j) {
+            struct vector pos = mechanism_relative_position(*m, footprint[j].u, footprint[j].v, 1);
+            if (cabinet_for_position(cabinet_map, pos) == 0)
+                return "solution contains a glyph outside of the cabinet";
+            if (vectors_equal(footprint[j], zero_vector))
+                break;
+        }
+    }
+
+    // check arms
+    for (uint32_t i = 0; i < solution->number_of_arms; ++i) {
+        struct mechanism *m = &solution->arms[i];
+        uint8_t base_cabinet = cabinet_for_position(cabinet_map, m->position);
+        if (base_cabinet == 0)
+            return "solution contains an arm outside of the cabinet";
+        int step = angular_distance_between_grabbers(m->type);
+        for (int direction = 0; direction < 6; direction += step) {
+            struct vector offset = u_offset_for_direction(direction);
+            struct vector pos = mechanism_relative_position(*m, offset.u, offset.v, 1);
+            uint8_t cabinet = cabinet_for_position(cabinet_map, pos);
+            if (cabinet == 0)
+                return "solution contains an arm outside of the cabinet";
+            else if (cabinet != base_cabinet)
+                return "solution contains an arm reaching across cabinet walls";
+        }
+    }
+
+    // check tracks
+    for (uint32_t i = 0; i < solution->track_table_size; ++i) {
+        struct vector position = solution->track_positions[i];
+        if (position.u == INT32_MIN && position.v == INT32_MIN)
+            continue;
+        if (cabinet_for_position(cabinet_map, position) == 0)
+            return "solution contains a track outside of the cabinet";
+    }
+
+    // check inputs/outputs
+    for (uint32_t i = 0; i < solution->number_of_inputs_and_outputs; ++i) {
+        struct input_output *io = &solution->inputs_and_outputs[i];
+        for (uint32_t j = 0; j < io->number_of_atoms; ++j)
+            if (cabinet_for_position(cabinet_map, io->atoms[j].position) == 0)
+                return "solution contains an input/output outside of the cabinet";
+    }
+
+    // check isolation
+    if (info->isolate_inputs_from_outputs) {
+        enum input_output_type *isolation_data = calloc(info->number_of_cabinets + 1, sizeof(enum input_output_type));
+        for (uint32_t i = 0; i < solution->number_of_inputs_and_outputs; ++i) {
+            struct input_output *io = &solution->inputs_and_outputs[i];
+            uint8_t cabinet = cabinet_for_position(cabinet_map, io->atoms->position);
+            isolation_data[cabinet] |= io->type;
+            if ((isolation_data[cabinet] & INPUT) && (isolation_data[cabinet] & OUTPUT))
+                return "solution breaks the input/output isolation constraint";
+        }
+        free(isolation_data);
+    }
+
+    return NULL;
 }
 
 bool decode_solution(struct solution *solution, struct puzzle_file *pf, struct solution_file *sf, const char **error)
@@ -571,6 +743,15 @@ bool decode_solution(struct solution *solution, struct puzzle_file *pf, struct s
             conduit->other_side_glyph_index = next->glyph_index;
         }
         solution->glyphs[conduit->glyph_index].conduit_index = i;
+    }
+    // production-only pass: enforce production constraints
+    if (pf->production_info) {
+        *error = check_production_constraints(solution, pf->production_info);
+        if (*error)
+            return false;
+    } else if (solution->number_of_conduits > 0) {
+        *error = "solution contains a conduit not defined in the puzzle file";
+        return false;
     }
     // decode arm tapes in one final pass.  this has to be another pass because
     // reset instructions depend on where track has been placed.

sim.h
@@ -559,6 +559,7 @@ const struct vector *glyph_footprint(enum mechanism_type type);
 struct vector u_offset_for_direction(int direction);
 struct vector v_offset_for_direction(int direction);
 int direction_for_offset(struct vector d);
+int angular_distance_between_grabbers(enum mechanism_type);
 struct vector mechanism_relative_position(struct mechanism m, int32_t du, int32_t dv, int32_t w);
 atom bond_direction(struct mechanism m, int32_t du, int32_t dv);
 

commit ec9904bd5ccd69015aaebcebe3b3109eeed4e300

Fix conduit edge case
The test `edge-cases/conduit-c544897394273089-12.solution` had been failing
since c34b318 "Improve production handling". Oops!

sim.c
@@ -287,7 +287,7 @@ static void apply_conduit(struct solution *solution, struct board *board, struct
                 atom a = conduit->atoms[base + k].atom;
                 atom *b = lookup_atom(board, p);
                 if (consume) {
-                    conduit->atoms[base + k].atom = *b;
+                    a = *b & ~OVERLAPS_ATOMS;
                     remove_atom(board, (struct atom_ref_at_position){ b, p });
                 } else {
                     // bonds are consumed even if the atoms aren't.