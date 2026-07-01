4b452103ce1d3f1adc520c6537c2c2f2e62dbaa0
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2026-07-01 19:38:41 +1200

    only call match_repeating_output_with_chain_atoms on cycles where the output matches 6 monomers

--- sim.c
+++ sim.c
@@ -1711,6 +1711,70 @@ static void consume_single_output(struct solution *solution, struct board *board
     io->number_of_outputs++;
 }
 
+static bool check_repeating_output_at_size(struct solution *solution, struct board *board, struct input_output *io, int output_index, struct molecule *molecule, uint32_t monomer_count)
+{
+    // The number of atoms above the target molecule size
+    // we decrement if an atom isn't part of the target molecule, and if it's ever below 0 then the molecule is too small
+    int ignorable_atoms = molecule->size - monomer_count*(io->number_of_original_atoms - 1);
+    if (ignorable_atoms < 0)
+        return false;
+
+    for (uint32_t i = 0; i < molecule->size; ++i) {
+        struct atom_ref_at_position a = molecule->atoms[i];
+        if (is_ignored_output_position(io, a.position, monomer_count)) {
+            --ignorable_atoms;
+            if (ignorable_atoms < 0)
+                return false;
+            continue;
+        }
+        atom target = 0;
+        for (uint32_t j = 0; j < io->number_of_original_atoms - 1; ++j) {
+            // look at the atom in the second monomer so the bonds are correct.
+            // relies on io->atoms and io->original_atoms having the same order.
+            struct atom_at_position monomer_atom = io->atoms[j + (io->number_of_original_atoms - 1)];
+
+            struct vector atom_position_polymer_space = polymer_position_from_global_position(io, a.position);
+            struct vector monomer_atom_position_polymer_space = polymer_position_from_global_position(io, monomer_atom.position);
+
+            // Compare horizontal position (in polymer space) modulo the monomer width, so we match any possible position of a monomer within the polymer
+            // (Vertical position is compared as normal)
+            if ((atom_position_polymer_space.u - monomer_atom_position_polymer_space.u) % io->monomer_width == 0 && atom_position_polymer_space.v == monomer_atom_position_polymer_space.v) {
+                // which monomer the current atom is in
+                // index is offset by 1 since we're comparing against the second monomer in the output molecule, and we want index relative to the first monomer
+                int32_t monomer_index = 1 + (atom_position_polymer_space.u - monomer_atom_position_polymer_space.u) / io->monomer_width;
+                // Check that this atom is actually part of a monomer in the n-mer that we're considering, and not a future monomer, or before the first monomer.
+                // This check should only fail in surg polymers, where an atom from monomer `n+1` appears directly to the left of an atom from monomer `n`.
+                // In all other cases out-of-bounds monomers will already be ignored by the is_ignored_output_position check.
+                if (monomer_index >= 0 && monomer_index < monomer_count) {
+                    target = monomer_atom.atom;
+                    break;
+                }
+            }
+        }
+        if (!target)
+            return false;
+        atom differences = *a.atom ^ target;
+
+        if (differences & REAL_BONDS) {
+            // bonds that lead to an ignored position are allowed
+            // This currently doesn't validate surg polymers properly, since we incorrectly expect floating bonds to empty but non-ignored positions
+            // (corresponding to atoms in monomers before and after the first and last expected monomer)
+            for (int dir = 0; dir < 6; ++dir) {
+                struct vector d = u_offset_for_direction(dir);
+                struct vector neighbor = { a.position.u + d.u, a.position.v + d.v };
+                if (is_ignored_output_position(io, neighbor, monomer_count))
+                    differences &= ~(BOND_LOW_BITS << dir);
+            }
+        }
+
+        if ((differences & ANY_ATOM) && !(target & VARIABLE_OUTPUT))
+            return false;
+        if (differences & REAL_BONDS)
+            return false;
+    }
+    return true;
+}
+
 static void check_repeating_output(struct solution *solution, struct board *board, struct input_output *io, int output_index, struct molecule *molecule)
 {
     // How many repetitions of the polymer we're matching against
@@ -1718,76 +1782,18 @@ static void check_repeating_output(struct solution *solution, struct board *boar
     // we start checking from one above the current highest validated count
     uint32_t monomer_count = 1 + io->number_of_outputs / io->outputs_per_repetition;
 
-    // match looping chains for infinite polymers if the first 6 products have been completed
+    // match looping chains for infinite polymers if the first 6 products have been completed and are present this cycle
     // TODO does checking this every cycle degrade performance? also does not checking this on the first cycle a polymer completes cause any issues?
-    if (monomer_count >= 7 && board->chain_mode == EXTEND_CHAIN)
+    if (board->chain_mode == EXTEND_CHAIN && monomer_count >= REPEATING_OUTPUT_REPETITIONS+1 && check_repeating_output_at_size(solution, board, io, output_index, molecule, REPEATING_OUTPUT_REPETITIONS))
         match_repeating_output_with_chain_atoms(board, io);
 
     // Repeatedly check increasing number of monomers until a match fails
     // This currently doesn't handle an edge case in surg polymer validation
     // where the game will run all of the first 6 monomer checks even if some fail, resulting in surg polymers validating in-game when they otherwise wouldn't
-    for(;; monomer_count++) {
-        // The number of atoms above the target molecule size
-        // we decrement if an atom isn't part of the target molecule, and if it's ever below 0 then the molecule is too small
-        int ignorable_atoms = molecule->size - monomer_count*(io->number_of_original_atoms - 1);
-        if (ignorable_atoms < 0)
-            return;
-
-        for (uint32_t i = 0; i < molecule->size; ++i) {
-            struct atom_ref_at_position a = molecule->atoms[i];
-            if (is_ignored_output_position(io, a.position, monomer_count)) {
-                --ignorable_atoms;
-                if (ignorable_atoms < 0)
-                    return;
-                continue;
-            }
-            atom target = 0;
-            for (uint32_t j = 0; j < io->number_of_original_atoms - 1; ++j) {
-                // look at the atom in the second monomer so the bonds are correct.
-                // relies on io->atoms and io->original_atoms having the same order.
-                struct atom_at_position monomer_atom = io->atoms[j + (io->number_of_original_atoms - 1)];
-
-                struct vector atom_position_polymer_space = polymer_position_from_global_position(io, a.position);
-                struct vector monomer_atom_position_polymer_space = polymer_position_from_global_position(io, monomer_atom.position);
-
-                // Compare horizontal position (in polymer space) modulo the monomer width, so we match any possible position of a monomer within the polymer
-                // (Vertical position is compared as normal)
-                if ((atom_position_polymer_space.u - monomer_atom_position_polymer_space.u) % io->monomer_width == 0 && atom_position_polymer_space.v == monomer_atom_position_polymer_space.v) {
-                    // which monomer the current atom is in
-                    // index is offset by 1 since we're comparing against the second monomer in the output molecule, and we want index relative to the first monomer
-                    int32_t monomer_index = 1 + (atom_position_polymer_space.u - monomer_atom_position_polymer_space.u) / io->monomer_width;
-                    // Check that this atom is actually part of a monomer in the n-mer that we're considering, and not a future monomer, or before the first monomer.
-                    // This check should only fail in surg polymers, where an atom from monomer `n+1` appears directly to the left of an atom from monomer `n`.
-                    // In all other cases out-of-bounds monomers will already be ignored by the is_ignored_output_position check.
-                    if (monomer_index >= 0 && monomer_index < monomer_count) {
-                        target = monomer_atom.atom;
-                        break;
-                    }
-                }
-            }
-            if (!target)
-                return;
-            atom differences = *a.atom ^ target;
-
-            if (differences & REAL_BONDS) {
-                // bonds that lead to an ignored position are allowed
-                // This currently doesn't validate surg polymers properly, since we incorrectly expect floating bonds to empty but non-ignored positions
-                // (corresponding to atoms in monomers before and after the first and last expected monomer)
-                for (int dir = 0; dir < 6; ++dir) {
-                    struct vector d = u_offset_for_direction(dir);
-                    struct vector neighbor = { a.position.u + d.u, a.position.v + d.v };
-                    if (is_ignored_output_position(io, neighbor, monomer_count))
-                        differences &= ~(BOND_LOW_BITS << dir);
-                }
-            }
-
-            if ((differences & ANY_ATOM) && !(target & VARIABLE_OUTPUT))
-                return;
-            if (differences & REAL_BONDS)
-                return;
-        }
+    while (check_repeating_output_at_size(solution, board, io, output_index, molecule, monomer_count)) {
         // if we validated an extra monomer, set the output count
         io->number_of_outputs = monomer_count * io->outputs_per_repetition;
+        monomer_count++;
     }
 }
 
